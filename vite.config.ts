bce4445c7d994815e232e6ec1d8007cdfea3b735
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2025-02-23 08:00:21 -0300

    Adiciona configuração condicional para ignorar o server durante build

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import fs from "node:fs";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    // Como o uso da camera depende um ambiente seguro,
-    // para desenvolvimento é necessário gerar certificados
-    // locais e configurar o vite para usá-los.
-    // https://vitejs.dev/config/server-options.html#server-https
-    https: {
-      key: fs.readFileSync("local-cert/key.pem"),
-      cert: fs.readFileSync("local-cert/cert.pem"),
+export default defineConfig(({ command }) => ({
+  ...(command === "serve" && {
+    server: {
+      // Como o uso da camera depende um ambiente seguro,
+      // para desenvolvimento é necessário gerar certificados
+      // locais e configurar o vite para usá-los.
+      // https://vitejs.dev/config/server-options.html#server-https
+      https: {
+        key: fs.readFileSync("local-cert/key.pem"),
+        cert: fs.readFileSync("local-cert/cert.pem"),
+      },
+      host: "0.0.0.0",
     },
-    host: "0.0.0.0",
-  },
-});
+  }),
+}));
